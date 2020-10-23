289f901251983f9b595b7b189e0248918205949d
Lucas Antonio Ramos Sartori <lucassartori@cookiecode.com.br>
2020-10-23 07:07:30 -0300

    added softdelete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::Allowlist
 
+  acts_as_paranoid
+
   devise :database_authenticatable,
          :encryptable,
          :registerable,
@@ -11,5 +13,6 @@ class User < ApplicationRecord
          :jwt_authenticatable,
          jwt_revocation_strategy: self
 
-
+  validates :name, :user_name, presence: true
+  validates :user_name, uniqueness: true
 end
