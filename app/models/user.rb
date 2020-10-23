8860151c811b17f501403d9b2622f9336f3a4a53
Lucas Antonio Ramos Sartori <lucassartori@cookiecode.com.br>
2020-10-23 06:55:54 -0300

    added devise user

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  include Devise::JWT::RevocationStrategies::Allowlist
+
+  devise :database_authenticatable,
+         :encryptable,
+         :registerable,
+         :recoverable,
+         :confirmable,
+         :validatable,
+         :trackable,
+         :jwt_authenticatable,
+         jwt_revocation_strategy: self
+
+
+end
