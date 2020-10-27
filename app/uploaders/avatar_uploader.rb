1b24d78f29e7c8c9fc569155cbc29789fc3f8b4c
Lucas Antonio Ramos Sartori <lucassartori@cookiecode.com.br>
2020-10-27 08:33:24 -0300

    fix carriewave

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -3,8 +3,6 @@
 class AvatarUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  storage :file
-
   def store_dir
     "uploads/avatars/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
--- app/uploaders/banner_uploader.rb
+++ app/uploaders/banner_uploader.rb
@@ -3,8 +3,6 @@
 class BannerUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  storage :file
-
   def store_dir
     "uploads/banners/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
